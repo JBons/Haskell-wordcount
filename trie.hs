b6e83b3f769a2302ba757ebd5a3c3a98169ec348
Juhani Bonsdorff <jbonsdorff@gmail.com>
2015-04-02 10:22:03 +0300

    Added toList and show to Trie
    Completed toList (part of Mapping class) and made Trie an instance of
    Show. toList appears to work. Show works butneeds some polish.

--- trie.hs
+++ trie.hs
@@ -8,6 +8,7 @@ import qualified Data.Map as M
 import Data.Word
 import qualified Data.ByteString.Char8 as B
 import Control.Monad
+import Text.Printf
 
 -- Will use existence of value to mark end of word
 data Trie c k v = Trie { value :: Maybe v, tails :: M.Map c (Trie c k v) }
@@ -28,7 +29,7 @@ class Ord k => Mapping m c k v where
     lookup :: m c k v -> k -> Maybe v
     update :: m c k v -> k -> (Maybe v -> v) -> m c k v 
 --    delete :: m c k v -> k -> M k v
---    toList :: m c k v -> ([k,v])
+    toList :: m c k v -> [(k,v)]
     empty  :: m c k v
 
 instance Mapping Trie Char String v where
@@ -45,4 +46,17 @@ instance Mapping Trie Char String v where
             Nothing  -> M.insert c newbranch (tails trie) where
                 newbranch = update empty cs f
              
-    empty = Trie { value = Nothing, tails = M.empty } 
+    empty = Trie { value = Nothing, tails = M.empty }
+
+    toList trie = concat $ map builder $ M.toList $ tails trie where     
+        builder :: (Char, Trie Char String v) -> [(String,v)]
+        builder (c,t) = case value t of
+                           Nothing -> prefix c $ toList t 
+                           Just v  -> ([c], v) : (prefix c $ toList t) 
+                       where prefix c ll = map (\(s,v) -> (c:s,v)) ll   
+
+instance Show v => Show (Trie Char String v) where
+    show t = let graph = toList t in 
+        (printf "Trie with %v key-value pairs, starting with:\n" (length graph)) ++ (show $ take 20 graph)
+
+-- TO DO: make show present the beginning as a table?
