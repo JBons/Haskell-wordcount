6eec402e46738fcdc3c20f0873183300b69fafac
Juhani Bonsdorff <jbonsdorff@gmail.com>
2015-04-06 19:10:09 +0300

    Add trie: size function; twc: display trie size
    Added trie size inspection and display for diagnistic purposes to
    dimension memory allocation for trie in the C code.
    The trie size function may be merged to main trie branch later, the
    modified twc likely not.

--- trie.hs
+++ trie.hs
@@ -69,6 +69,5 @@ instance (Show v, Show [c], Ord c) => Show (Trie c v) where
                  summary = printf "Trie with %v key-value pairs, starting with:\n" (length graph)
                  display = concat  . fmap ( \(k,v) ->  printf "%15s :   %4v \n" (show k) (show v) )
 
-
-
-
+size :: Ord c => Trie c v -> Int
+size t = 1 + sum (fmap (size.snd) $ M.toList $ tails t)
