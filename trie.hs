bcc9eb4c448a23a89d5b583719473662d087a36b
Juhani Bonsdorff <jbonsdorff@gmail.com>
2015-04-19 11:57:56 +0300

    Add traversable instance to Trie
    Added traversable instance, and hence via defaults also working Foldable
    instance. First testing indicates that works as should.

--- trie.hs
+++ trie.hs
@@ -4,11 +4,11 @@
 
 module Trie where
 
-import Prelude hiding (lookup)
+import Prelude hiding (lookup, sequence)
 import qualified Data.Map as M
 import Data.Word
-import Control.Monad
-import qualified Control.Applicative as A
+import Control.Monad hiding (sequence)
+import Control.Applicative ((<$>), (<*>), pure, Applicative) 
 import qualified Data.Foldable as F
 import Data.Traversable
 import Text.Printf
@@ -60,10 +60,24 @@ fromList = foldl (\ t (k,v) -> add v t k ) empty
 
 
 instance Ord c => Traversable (Trie c) where
-   traverse f t = undefined -- HUOM
+   traverse f t = Trie <$> root <*> children  where
+       root     = sequenceA( fmap f (value t) )
+       children = sequenceA $ M.map (traverse f) (tails t)
 
 instance Ord c => F.Foldable (Trie c) where
     foldMap = foldMapDefault
+
+instance Ord c => Monad (Trie c) where
+    (>>=) t f = let tl = toList t in (fromList.concat) (map g tl) where
+       g (k,v) = map (\(a,b) -> (k++a, b)) (toList (f v))
+    return v = add v empty []    
+
+instance Ord c=> Applicative (Trie c) where
+    (<*>) = ap
+    pure  = return
+
+instance Ord c => Functor (Trie c) where
+    fmap f t = t >>= (return.f) 
 
 instance (Show v, Show [c], Ord c) => Show (Trie c v) where
     show t = summary ++ (display $ take 15 graph) where   
@@ -71,23 +85,15 @@ instance (Show v, Show [c], Ord c) => Show (Trie c v) where
                  summary = printf "Trie with %v key-value pairs, starting with:\n" (length graph)
                  display = concat  . fmap ( \(k,v) ->  printf "%15s :   %4v \n" (show k) (show v) )
 
+
 {- Made T=(Trie c) into a monad with t >>= f defined as follows:
 -  
 - for each key k in t with corresponding value v, take the keys ks of f v. Form new keys k's = (k ++ ks) 
 - by concatenating. Replace the key k in t with the keys k's and give them values from (f v).
 -}
 
-instance Ord c => Monad (Trie c) where
-    (>>=) t f = let tl = toList t in (fromList.concat) (map g tl) where
-       g (k,v) = map (\(a,b) -> (k++a, b)) (toList (f v))
-    return v = add v empty []    
 
-instance Ord c=> A.Applicative (Trie c) where
-    (<*>) = ap
-    pure  = return
 
-instance Ord c => Functor (Trie c) where
-    fmap f t = t >>= (return.f) 
 
 -- OLD code
 
