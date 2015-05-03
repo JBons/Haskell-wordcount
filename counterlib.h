a4ec1d72ad2f4ca5a76f7200a32afdd6ed96d820
Juhani Bonsdorff <jbonsdorff@gmail.com>
2015-05-03 23:46:42 +0300

    Add Haskell interface to C-code
    Added a haskell program to interface the C code trie-based word counter.
    Turned the C code into library source to be compiled into a dynamic libabry.
    Built custom marshaling code in Haskell to call the C code. It compiles
    and runs without crashing. However, results are garbled - as C code has
    not chnaged, assume issue is in marshalling either way. Have to debug
    with smaller input text.

---
+++ counterlib.h
@@ -0,0 +1,24 @@
+/* Headers for the counting function */
+
+#define MAXWORDLENGTH 20
+
+/* Declaration of word count structure */
+struct Word
+{
+    char word[MAXWORDLENGTH];
+    int count;
+};
+typedef struct Word Word;
+
+/* The main counting function, the only one exported */
+int counts(char*, Word**, Word*); 
+
+
+/* Need to check compilation / linking options:
+ *
+ * gcc -fPIC -c filename.c
+ *
+ * may be right? */
+
+
+
---
+++ ctwc.hs
@@ -0,0 +1,76 @@
+{- Compilation: gcc -dynamiclib -fPIC -o lib.so counterlib.o
+ - ghci lib.so
+ - ghc ctwc.hs lib.so
+ -} 
+
+
+
+
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+module Main where
+
+import Prelude hiding (words)
+import Data.Char hiding (isSpace)
+import Data.List (reverse)
+import GHC.Exts (sortWith)
+import Control.Monad (forM, liftM)
+import System.Environment
+import System.TimeIt
+import Text.Printf
+import GHC.Exts hiding (Word)
+
+import Foreign.C.String
+import Foreign.Storable
+import Foreign.C.Types
+import Foreign.Ptr
+import Foreign.Marshal.Alloc
+import Foreign.Marshal.Array
+
+data Word = Word { count :: Int
+                 , word  :: String }
+
+instance Storable Word where
+    peek ptr = do
+        let intP = (castPtr ptr)   :: Ptr CInt
+        let chrP = (plusPtr ptr 4) :: CString
+        cc   <- peek intP
+        let count = fromIntegral cc
+        word <- peekCString chrP
+        return $ Word {count = count, word = word}
+    poke ptr w = do return () -- do nothing
+    sizeOf _ = 24
+    alignment _ = 4
+
+foreign import ccall "counterlib.h counts" c_counts :: CString -> Ptr(Ptr Word) -> Ptr () -> IO CInt
+
+-- COMPILES BUT MISREADS THE DATA
+-- Words of a text together with their frequencies, sorted highest->lowest
+wordFreqs :: String ->  IO [(String,Int)]  
+wordFreqs s = do
+    cs   <- newCAString s                            -- Forcing use of 8-bit ASCII strings
+    wpa  <- mallocArray 50000 :: IO (Ptr (Ptr Word)) -- Room for return array of pointers to words 
+    heap <- mallocBytes(50000 * 24)                  -- Room for the Word structures
+    wc <- c_counts cs wpa heap
+    let size = fromIntegral wc
+    plist <- peekArray size wpa
+    wordlist <- mapM peek plist
+    let raw = map (\w -> (word w, count w)) wordlist
+    --let result = reverse $ sortWith snd raw
+    return raw --fix back after debugging
+
+
+main = timeIt proc --get running time
+
+--Actual main UI task
+proc = do args <- getArgs
+          let n = (read $ args!! 0) :: Int
+          let filename  = args!! 1
+          text   <- readFile filename
+          result <- wordFreqs text
+          display $ take n result
+
+--Pretty-print the results
+display xs = mapM putStrLn $ fmap disp xs where
+    disp (x,y) = printf "%15s :   %4d" x y 
+
